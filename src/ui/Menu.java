ee9097d5dc0ae38bdb85369a5e5b3b110b6def55
Nathan Wagner <nwkaue1@gmail.com>
2023-06-19 21:25:08 -0300

    1.1

--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -4,61 +4,47 @@ import estoque.Estoque;
 import estoque.Produto;
 import input.Input;
 
-public class Menu
-{
-    Input input = new Input();
+import java.util.InputMismatchException;
+
+public class Menu {
     Estoque estoque = new Estoque();
 
-    public void menuInicial()
-    {
-        while(true)
-        {
+    public void menuInicial() {
+        while(true) {
             System.out.println("****************\nESTOQUE DUS GURI\n****************");
-
             System.out.println("\n\n1. INSERIR PRODUTO\n2. ADICIONAR QUANTIDADES DE PRODUTOS NO ESTOQUE\n3. RETIRAR QUANTIDADE DE PRODUTOS NO ESTOQUE\n4. LISTAR TODOS OS PRODUTOS\n5. LISTAR PRODUTOS QUE ESTAO ABAIXO DO ESTOQUE\n6. SAIR");
-    
+
             escolhas();
         }
     }
 
-    public void escolhas()
-    {
-        switch (input.inputInt(""))
-        {
-            case 1:
-                adicionaNovoProduto();
-                break;
-            case 2:
+    public void escolhas() {
+        switch (Input.inputInt("")) {
+            case 1 -> adicionaNovoProduto();
+            case 2 -> {
                 Produto produtoQntAdicionada = manipulacaoDeQuantidades();
                 adicionarQuantidade(produtoQntAdicionada);
-                break;
-            case 3:
+            }
+            case 3 -> {
                 Produto produtoQntRemovida = manipulacaoDeQuantidades();
                 retirarQuantidade(produtoQntRemovida);
-                break;
-            case 4:
-                estoque.listarProdutos();
-                break;
-            case 5:
+            }
+            case 4 -> estoque.listarProdutos();
+            case 5 -> {
                 //mostraProdutosAbaixo();
-                estoque.produtosAbaixo();;
-                break;
-            case 6:
-                System.exit(0);
-                break;
-            default:
-                System.out.println("Digite um número válido! ");
-                break;
+                estoque.produtosAbaixo();
+            }
+            case 6 -> System.exit(0);
         }
     }
 
 
     public void adicionaNovoProduto()
     {
-        String nome = input.inputString("Inserir nome do Produto");
-        int qnt = input.inputInt("Inserir quantidade do Produto");
-        int qntMinima = input.inputInt("Inserir quantidade minima do Produto");
-        double preco = input.inputDouble("Preco do Produto");
+        String nome = Input.inputString("Inserir nome do Produto");
+        int qnt = Input.inputInt("Inserir quantidade do Produto");
+        int qntMinima = Input.inputInt("Inserir quantidade minima do Produto");
+        double preco = Input.inputDouble("Preco do Produto");
 
         Produto produto = new Produto(nome, qnt, qntMinima, preco);
         estoque.inserirNovoProduto(produto);
@@ -70,19 +56,19 @@ public class Menu
         System.out.println("Qual produto voce gostaria de selecionar? (SELECIONE PELO CODIGO)");
         estoque.listarProdutos();
 
-        return estoque.selecionarProduto(input.inputInt(""));
+        return estoque.selecionarProduto(Input.inputInt(""));
     }
 
     public void adicionarQuantidade(Produto produtoSelecionado)
     {
-        int quantidade = input.inputInt("Quantidade do produto " +produtoSelecionado.getNome()+ " a ser adionada: ");
+        int quantidade = Input.inputInt("Quantidade do produto " +produtoSelecionado.getNome()+ " a ser adionada: ");
         estoque.inserirQuantidadeEstoque(produtoSelecionado, quantidade);
         System.out.println("ADICIONADO(A) " +quantidade+ " UN PARA " +produtoSelecionado.getNome().toUpperCase() +"\nTOTAL: " +produtoSelecionado.getQnt());
     }
 
     public void retirarQuantidade(Produto produtoSelecionado)
     {
-        int quantidade = input.inputInt("Quantidade do produto " +produtoSelecionado.getNome()+ " a ser removida: ");
+        int quantidade = Input.inputInt("Quantidade do produto " +produtoSelecionado.getNome()+ " a ser removida: ");
         estoque.retirarQuantidadeEstoque(produtoSelecionado, quantidade);
         System.out.println("RETIRADO(A) " +quantidade+ " UN PARA " +produtoSelecionado.getNome().toUpperCase() +"\nTOTAL: " +produtoSelecionado.getQnt());
     }
